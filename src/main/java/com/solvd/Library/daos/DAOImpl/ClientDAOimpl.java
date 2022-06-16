3c8f9f63a600e565cf7278ec63ccd0f3a406ad19
FPanichelli <fpanichelli@solvd.com>
2022-06-16 18:25:55 -0300

    branch update

---
+++ src/main/java/com/solvd/Library/daos/DAOImpl/ClientDAOimpl.java
@@ -0,0 +1,4 @@
+package com.solvd.Library.daos.DAOImpl;
+
+public class ClientDAOimpl {
+}
--- src/main/java/com/solvd/Library/entitie/Book.java
+++ src/main/java/com/solvd/Library/entitie/Book.java
@@ -71,7 +71,7 @@ public class Book {
                 + tittle
                 + "\nAuthor: "
                 + author
-                + "\nCategory: "
+                + "\nGenre: "
                 + genre
                 ;
     }
